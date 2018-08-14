cc242baf78368b9480e6cb41a722cbb950e104eb
Myraha Harmon-Arias <myrahaa@gmail.com>
2018-08-14 14:47:08 -0400

    update score functions [issue 21]

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+//score variables
+let botScore = 0
+let playerScore = 0
+
+//track botScore
+function increaseBotScore(){
+    botScore+=1;
+    document.getElementById("computerScore").innerHTML = botScore;
+    displayCompleteMessage('You Lose!')
+}
+
+//track playerScore
+function increasePlayerScore(){
+    playerScore+=1;
+    document.getElementById("humanScore").innerHTML = playerScore;
+    displayCompleteMessage('You Win!')
+}
+
 // function to display win or lost message
 function displayCompleteMessage(msg){
 	document.getElementById("status").innerHTML=msg;
